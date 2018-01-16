b2c004a3727a92bf1224b6560847e602216cade5
zaaler <zachary.allen@colorado.edu>
2018-01-16 16:54:29 -0700

    Removing Radiance folder after added branch

--- Radiance/util/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Creates a named pipe in /tmp/ for reading heartbeat information,
-# then starts the radiance process
-
-sudo service watchdog start
-
-mkfifo /tmp/stdout_log
-sudo /home/pi/RADIANCE-main/radiance 2>1 | tee /tmp/stdout_log
