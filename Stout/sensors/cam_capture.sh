b3abd6e40e459ae14f9936311cb62c99bbb964a7
CU-STOUT <matthew.funk@colorado.edu>
2018-02-08 16:24:59 -0700

    Simple bash script to capture image

---
+++ Stout/sensors/cam_capture.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TITLE: Camera Capture Function
+# AUTHOR: Zack Allen
+# EDITED: 2/8/18
+
+# INPUTS: First argument to function is filename ($1)
+
+fswebcam -r 1920x1080 --jpeg 95 -F 1 $1
